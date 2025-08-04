ef11b53566fd2dcc7aa7a213780c9ff5720cfab5
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2025-08-04 23:52:29 +0000

    Update metadata and bare minimum homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,31 @@
 import { Button } from "@/components/ui/button";
+import { GraduationCap, Hammer } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-[24px] row-start-2 items-center sm:items-start">
+        <h1 className="text-4xl sm:text-5xl font-bold flex items-center gap-4">
+          <GraduationCap className="w-20 h-20" />
+          Overpass
+        </h1>
+        <p className="text-lg text-muted-foreground">
+          A student data management system for SHS in Ghana.
+        </p>
         <Button>Start now</Button>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://nkrumahsarpong.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
+          <Hammer className="w-6 h-6" />
+          Built by Emmanuel Nkrumah Sarpong
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
       </footer>
     </div>
   );
